316712c9c34a69089537c1793d60b2ee5cc0ac1e
IOConcept <123223096+IOConcept@users.noreply.github.com>
2023-03-12 06:29:36 -0500

    Update main.py
    THIS WORKS!

--- main.py
+++ main.py
@@ -1,51 +1,42 @@
-import csv
-import os.path
-from random import choice
+import os
 
-from nltk.chat.util import Chat, reflections
+def read_library():
+    # Create the library file if it doesn't exist
+    if not os.path.exists("library.txt"):
+        with open("library.txt", "w") as f:
+            f.write("What is your name?:My name is A.L.I.C.E.\nHow old are you?:I was activated in 1995, so I am over 25 years old.\nWhat can you do?:I can help you with a variety of tasks, from answering questions to playing games.")
 
-# Check if library file exists, create one if not
-if not os.path.isfile("library.csv"):
-    with open("library.csv", mode="w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["question", "response"])
-        writer.writerow(["What is Starbeam One?", "Starbeam.one is a digital marketplace that enables creators to issue and sell digital assets, including music, videos, images, and software. It allows artists to promote and sell their work directly to consumers, and allows consumers to purchase and collect unique and valuable digital assets. The platform is designed to provide a secure and decentralized marketplace, with blockchain technology used to ensure transparency, immutability, and authenticity of all transactions. Additionally, the platform features an AI-powered chatbot named A.L.I.C.E, who serves as a guide and a helpful resource for users navigating the platform."])
+    # Read the library file
+    with open("library.txt", "r") as f:
+        library = f.read().split("\n")
 
-# Load library from CSV file
-with open("library.csv", newline="") as f:
-    reader = csv.reader(f)
-    next(reader) # skip header row
-    library = [{"question": row[0], "response": row[1]} for row in reader]
+    # Create a dictionary of questions and answers
+    pairs = {}
+    for pair in library:
+        if pair:
+            question, answer = pair.split(":")
+            pairs[question.lower()] = answer
 
-# Create pairs from library
-pairs = [(pair["question"], pair["response"]) for pair in library]
+    return pairs
 
-# Add default reflection pairs
-reflections[""] = ""
+def chat():
+    # Load library
+    library = read_library()
 
-# Create chat bot
-chatbot = Chat(pairs, reflections)
+    # Create list of questions
+    questions = list(library.keys())
 
-def main():
-    print("A.L.I.C.E: Hi, I'm A.L.I.C.E. How can I help you today?")
+    print("A.L.I.C.E.: Hi, I'm A.L.I.C.E. How can I help you today?")
     while True:
         user_input = input("You: ")
-        if not user_input:
-            print("Available questions:")
-            for i, pair in enumerate(library):
-                print(f"{i+1}. {pair['question']}")
-            continue
-        if user_input.isdigit() and int(user_input) in range(1, len(library)+1):
-            response = library[int(user_input)-1]["response"]
+        response = library.get(user_input.lower())
+        if response:
+            print("A.L.I.C.E.:", response)
         else:
-            response = chatbot.respond(user_input)
-            if not response:
-                print("A.L.I.C.E.: I'm sorry, I didn't understand your question. Here are the available questions:")
-                for i, pair in enumerate(library):
-                    print(f"{i+1}. {pair['question']}")
-                continue
-        print("A.L.I.C.E:", response)
+            print("A.L.I.C.E.: I'm sorry, I don't have an answer for that. Here are some available questions:")
+            for question in questions:
+                print(f"- {question.capitalize()}")
 
 if __name__ == "__main__":
-    print("A.L.I.C.E. v6.1")
-    main()
+    print("A.L.I.C.E. v8.0 Text Library Edition")
+    chat()
